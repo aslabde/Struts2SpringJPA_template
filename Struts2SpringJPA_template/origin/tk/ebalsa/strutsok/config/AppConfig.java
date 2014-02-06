f178b375c45254144722402d4f9c14e8f705cc6d
aslabde <aslabde@gmailcom>
2014-02-06 01:26:41 +0100

    Template complete!! ; )

--- Struts2SpringJPA_template/origin/tk/ebalsa/strutsok/config/AppConfig.java
+++ Struts2SpringJPA_template/origin/tk/ebalsa/strutsok/config/AppConfig.java
@@ -1,32 +1,28 @@
 package tk.ebalsa.strutsok.config;
 
-import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
-
 import java.sql.SQLException;
 
+import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate3.HibernateExceptionTranslator;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-import tk.ebalsa.strutsok.bo.BookBoImpl;
+import com.jolbox.bonecp.BoneCPDataSource;
+
 import tk.ebalsa.strutsok.persistence.BookRepository;
 
 @Configuration
@@ -34,13 +30,23 @@ import tk.ebalsa.strutsok.persistence.BookRepository;
 	includeFilters = @ComponentScan.Filter(value = {BookRepository.class}, type = FilterType.ASSIGNABLE_TYPE))
 @EnableTransactionManagement
 @ComponentScan("tk.ebalsa.strutsok")
+@PropertySource("classpath:/tk/ebalsa/strutsok/config/app.properties")
 public class AppConfig {
-
+	
+	  @Resource
+	  private Environment environment;
+	
 	 @Bean
-	  public DataSource dataSource() throws SQLException {
+	  public DataSource dataSource()  {
 
-	    EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-	    return builder.setType(EmbeddedDatabaseType.H2).build();
+		 BoneCPDataSource dataSource = new BoneCPDataSource();
+
+	        dataSource.setDriverClass(environment.getRequiredProperty("jdbc.driverClassName"));
+	        dataSource.setJdbcUrl(environment.getRequiredProperty("jdbc.url"));
+	        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+	        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+
+	        return dataSource;
 	  }
 
 	  @Bean
